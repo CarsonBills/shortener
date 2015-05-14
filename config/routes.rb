2cfc3a3198c6bec3df324e0564c64257402d0daa
CarsonBills <CarsonMBills@Gmail.com>
2015-05-14 16:13:29 -0400

    passing tests for links controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/:id" => "links#show", :as => "link"
   root "links#new", :as => "new_link"
   post "/" => "links#create", :as => "links"
+  get "/top" => "links#index"
 end
