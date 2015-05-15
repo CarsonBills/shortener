3936bfa0236334201a7951c48c0db68e4b00a910
CarsonBills <CarsonMBills@Gmail.com>
2015-05-15 10:07:45 -0400

    deletes irrelevant test

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -2,13 +2,6 @@ require "spec_helper"
 require "rails_helper"
 
 describe LinksController do
-  describe "GET index" do
-    it "renders index template" do
-      get :index
-      expect(response).to render_template("index")
-    end
-  end
-
   describe "GET new" do
     it "renders new template and Home" do
       get :new
